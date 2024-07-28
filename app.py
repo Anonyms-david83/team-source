7524dd4bc99d301ce2b7a1f8b8f32f0c306fbb0e
Eilish Cuthbert <erfansafarali98@gmail.com>
2024-07-28 14:26:42 +0330

    article app initiallized

--- app.py
+++ app.py
@@ -12,3 +12,8 @@ def index():
 def product_detail(shop_id , shop_name):
 
     return render_template('detail.html' , product_id = shop_id , product_name = shop_name)
+
+
+@app.route('/article')
+def article():
+    return render_template('article.html')
