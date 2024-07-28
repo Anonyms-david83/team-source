a8eb419f5123b582cee2ee1b7afba364e82843d8
Eilish Cuthbert <erfansafarali98@gmail.com>
2024-07-28 14:26:01 +0330

    shop app initiallized

--- app.py
+++ app.py
@@ -6,3 +6,9 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('home.html')
+
+
+@app.route('/shop/<int:shop_id>/<string:shop_name>')
+def product_detail(shop_id , shop_name):
+
+    return render_template('detail.html' , product_id = shop_id , product_name = shop_name)
