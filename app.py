6bc486298117b0a36a61e835e6a96a5b47c4adc0
Eilish Cuthbert <erfansafarali98@gmail.com>
2024-07-28 15:12:57 +0330

    database configrations initialized

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask , render_template , request
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///sqlite.db"
+db = SQLAlchemy(app)
+
+with app.app_context():
+    db.create_all()
 
 
 @app.route('/')
