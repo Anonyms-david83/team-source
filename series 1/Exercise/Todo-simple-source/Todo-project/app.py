df33a182725b2edc941e63d38f87c3ac21556c2f
Eilish Cuthbert <erfansafarali98@gmail.com>
2024-07-31 14:50:28 +0330

    sqlalchemy initialized

--- series 1/Exercise/Todo-simple-source/Todo-project/app.py
+++ series 1/Exercise/Todo-simple-source/Todo-project/app.py
@@ -1,7 +1,11 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
 
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def hello_world():
