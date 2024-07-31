4986b6ce71ef8596290448456c64bb533d4bea73
Eilish Cuthbert <erfansafarali98@gmail.com>
2024-07-31 14:55:14 +0330

    flask project bugs fixed and now ready for more future developments

--- series 1/Exercise/Todo-simple-source/Todo-project/app.py
+++ series 1/Exercise/Todo-simple-source/Todo-project/app.py
@@ -1,16 +1,31 @@
-from flask import Flask
+from flask import Flask , render_template , request
 from flask_sqlalchemy import SQLAlchemy
 
+
+##############################################################
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todos.db'
+
+
+
+####################################################################
+
+
 db = SQLAlchemy(app)
 
 with app.app_context():
     db.create_all()
 
+
+#########################################################################
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('home.html')
 
 
+
+
+###########################################################################
 if __name__ == '__main__':
     app.run()
