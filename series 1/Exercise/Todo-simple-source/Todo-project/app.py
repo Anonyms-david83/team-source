fe8094c113501931c26c2b4a66b79df4d3b68d70
Eilish Cuthbert <erfansafarali98@gmail.com>
2024-07-31 15:16:05 +0330

    some errors in with context manager fixed and the main table of toods in front end synced with the data coming fromt the backend

--- series 1/Exercise/Todo-simple-source/Todo-project/app.py
+++ series 1/Exercise/Todo-simple-source/Todo-project/app.py
@@ -1,6 +1,6 @@
 from flask import Flask , render_template , request
 from flask_sqlalchemy import SQLAlchemy
-
+from datetime import datetime
 
 ##############################################################
 
@@ -14,18 +14,41 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todos.db'
 
 db = SQLAlchemy(app)
 
-with app.app_context():
+
+
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True , unique=True)
+    body = db.Column(db.String(100))
+    date = db.Column(db.DateTime, default=datetime.now)
+    status = db.Column(db.Boolean, default=True)
+
+
+
+with app.app_context(): #with context manager must be always after the class of models
     db.create_all()
 
 
+
 #########################################################################
-@app.route('/')
-def hello_world():
-    return render_template('home.html')
+@app.route('/' , methods=['GET', 'POST'])
+def index():
+    if request.method == 'GET':
+        todos = Todo.query.all()
+        return render_template('home.html' , todos = todos)
+
+
+    elif request.method == 'POST':
+        todo_body = request.form['input_body']
+        new_todo = Todo(body=todo_body , status=True)
+        db.session.add(new_todo)
+        db.session.commit()
+        return render_template('home.html')
+
 
 
 
 
 ###########################################################################
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
