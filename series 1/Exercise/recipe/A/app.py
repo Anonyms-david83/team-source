0b170bc334e8bf651f40259d16ba6d5946ae40fc
erfan <computerprogrammeralexsafar@gmail.com>
2024-09-12 03:22:31 +0330

    contact us added in index and contactus page

--- series 1/Exercise/recipe/A/app.py
+++ series 1/Exercise/recipe/A/app.py
@@ -1,8 +1,8 @@
 from flask import Flask , render_template , url_for , request , flash , redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
-from wtforms import StringField , FileField , IntegerField , TextAreaField
-from wtforms.validators import DataRequired , Length
+from wtforms import StringField , FileField , IntegerField , TextAreaField , EmailField
+from wtforms.validators import DataRequired , Length , Email
 from flask_wtf.file import FileAllowed
 from werkzeug.utils import secure_filename
 import  os
@@ -34,7 +34,14 @@ class Recipe(db.Model):
     img_url = db.Column(db.String(50), nullable=False , unique=True)
     add_date = db.Column(db.Date, nullable=False, default=datetime.now)
 
-
+class Ticket(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), nullable=False)
+    lname = db.Column(db.String(20), nullable=False)
+    description = db.Column(db.String(80), nullable=False)
+    date = db.Column(db.Date, nullable=False , default=datetime.now)
+    phone = db.Column(db.Integer, nullable=False)
+    email = db.Column(db.String(80), nullable=False)
 
 with app.app_context():
     db.create_all()
@@ -50,6 +57,12 @@ class RecipeRegisterationForm(FlaskForm):
     cost = IntegerField(label='Cost', validators=[DataRequired(message='این فیلد نمیتواند خالی باشد')])
     img = FileField(label='Image URL', validators=[DataRequired(message='این فیلد نمیتواند خالی باشد') , FileAllowed(['jpg', 'png','jpeg'], message='فرمت ورودی فقط میتواند jpg یا png باشد ')])
 
+class TicketForm(FlaskForm):
+    name = StringField(label='Your name' , validators=[DataRequired(message='این فیلد نمیتواند خالی باشد') , Length(min=2 , max=20)])
+    lname = StringField(label='Your lname' , validators=[DataRequired(message='این فیلد نمیتواند خالی باشد') , Length(min=2 , max=20)])
+    description = StringField(label='Your request' , validators=[DataRequired(message='این فیلد نمیتواند خالی باشد') , Length(min=2 , max=100)])
+    phone = IntegerField(label='Your phone' , validators=[DataRequired(message='این فیلد نمیتواند خالی باشد مرد مومن')])
+    email = EmailField(label='your email' , validators=[DataRequired(message='این فیلد نمیتواند خالی باشد') , Email(message='ایمیل وارد شده مشکل داره')])
 
 ########################################################################################################################
 
@@ -57,8 +70,9 @@ class RecipeRegisterationForm(FlaskForm):
 def index():
     template_name = 'index.html'
     recipes = Recipe.query.all()
+    form_class = TicketForm()
 
-    return render_template(template_name , recipes = recipes)
+    return render_template(template_name , recipes = recipes , form = form_class)
 
 
 @app.route('/register' , methods=['GET', 'POST'])
@@ -110,6 +124,8 @@ def delete(recipe_id):
 
     try:
         db.session.delete(target_recipe)
+        img_path = target_recipe.img_url
+        os.remove(img_path)
         db.session.commit()
         flash('با موفقیت غذای بد مزه شما حذف شد' , 'success')
         return redirect(url_for('index'))
@@ -117,6 +133,33 @@ def delete(recipe_id):
         flash('اینقدر غذات نحذ بود پاک نشد دوباره امتحان کن' , 'success')
         return redirect(url_for('index'))
 
+
+@app.route('/contactus' , methods = ['POST' , 'GET'])
+def contactus():
+    template_name = 'contact.html'
+    form_class = TicketForm()
+
+    if request.method == 'GET':
+        return  render_template(template_name , form = form_class)
+
+    elif request.method == 'POST':
+        if form_class.validate_on_submit():
+            name = form_class.name.data
+            lname = form_class.lname.data
+            phone = form_class.phone.data
+            email = form_class.email.data
+            description = form_class.description.data
+
+            new_ticket = Ticket(name=name , lname=lname , phone=phone , email=email , description=description)
+            db.session.add(new_ticket)
+            db.session.commit()
+            flash('تیکت با موفقیت ثبت شد ' , 'success')
+
+            return redirect(url_for('index'))
+
+        else :
+            return render_template(template_name, form=form_class)
+            flash('تیکت مشکل داشت دوباره امتحان کنید' , 'error')
 
 ########################################################################################################################
 
